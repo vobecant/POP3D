754a5c5713ac00a9e816cc3ff2137b2dca7204e8
Antonin <a.vobecky@gmail.com>
2023-12-27 09:53:53 +0100

    mini

---
+++ config/_base_/dataset_pop3d_mini.py
@@ -0,0 +1,41 @@
+dataset_params = dict(
+    version="v1.0-trainval",
+    ignore_label=255,
+    fill_label=0,
+    fixed_volume_space=True,
+    label_mapping="./config/label_mapping/nuscenes-agnostic.yaml",
+    max_volume_space=[51.2, 51.2, 3],
+    min_volume_space=[-51.2, -51.2, -5],
+    dataset_type="ImagePoint_NuScenes_withFeatures",
+    features_type="k",
+    features_path=...,
+    projections_path=...,
+    class_agnostic=True
+)
+
+train_data_loader = dict(
+    data_path="data/nuscenes/",
+    imageset="./data/nuscenes_infos_train_mini.pkl",
+    batch_size=1,
+    shuffle=True,
+    num_workers=1,
+)
+
+val_data_loader = dict(
+    data_path="data/nuscenes/",
+    imageset="./data/nuscenes_infos_val_mini.pkl",
+    batch_size=1,
+    shuffle=False,
+    num_workers=1,
+)
+
+test_data_loader = dict(
+    data_path="data/nuscenes/",
+    imageset="./data/nuscenes_infos_test.pkl",
+    batch_size=1,
+    shuffle=False,
+    num_workers=1,
+)
+
+unique_label = [0, 1]
+class_weights_path = "./data/class_weights.pth"
