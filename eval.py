95abd09f7011916902bcea6b828864721a5fb0ae
vobecant <47218173+vobecant@users.noreply.github.com>
2024-01-02 15:13:52 +0100

    Update eval.py

--- eval.py
+++ eval.py
@@ -620,7 +620,8 @@ def main(local_rank, args):
         val_miou_pts_clip_wRoad = CalMeanIou_pts_clip_wRoad._after_epoch(
             log_wandb=log2wandb, tag="miou_pts_clip_wRoad/val", step=None)
 
-        print(f'val_miou_vox_agn (evaluate voxel agnostic segmentation everywhere): {val_miou_vox_all}')
+        #print(f'val_miou_vox_agn (evaluate voxel agnostic segmentation everywhere): {val_miou_vox_all}')
+        print(f'val_miou_vox_agn: {val_miou_vox_all}')
         print(f'val_miou_vox_clip_all: {val_miou_vox_clip_all}')
 
     time_val_ep_elapsed = time.time() - time_val_ep_start
