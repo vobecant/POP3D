7b606119fcef11e2d09387cabd1f451ebd903bd1
vobecant <47218173+vobecant@users.noreply.github.com>
2024-01-22 13:38:32 +0100

    Update download_retrieval_benchmark.sh

--- scripts/download_retrieval_benchmark.sh
+++ scripts/download_retrieval_benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget -P ./data https://data.ciirc.cvut.cz/public/projects/2023POP3D/retrieval_benchmark.tar.gz
 mkdir -p ./data
+wget -P ./data https://data.ciirc.cvut.cz/public/projects/2023POP3D/retrieval_benchmark.tar.gz
 cd ./data 
 tar -xzf retrieval_benchmark.tar.gz
 mv retrieval_benchmark_release retrieval_benchmark
